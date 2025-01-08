d476e3b1690c17494faa16bd376dda4eb065df0f
FarzamN <frzamn64ml@gmail.com>
2025-01-08 15:19:27 +0500

    vercel added

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
 import express from "express";
+import session from "express-session";
+import {config} from "dotenv";
+import passport from "passport";
+
+config();
 
 const app = express();
 
+app.use(session({
+    secret: process.env.SECRET_KEY,
+    resave: false,
+    saveUninitialized: true,
+
+}), passport.initialize(), passport.session());
+
 app.get("/", (req, res) => {
-  res.send("Hello World");
+    req.session.count ++
+    console.log(req.session);
+    res.send("Please signin or register");
 });
 
 app.listen(3000, () => {
